ad288fa74187147607847c497ce2dcc702bd7658
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-07-05 16:40:46 +0200

    commit

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 from pyscript import when
+from ultralytics import hub
 
-while True:
-    print("loop")
-
+hub.login("86d48192211ece7b9fa5e0ff811f82e378538ca65c")
 @when("click", "button")
 def hello():
     print("hello")
