f6605f64944207f6e9bc6b9166e3e74d1b36a642
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-07-06 12:35:38 +0200

    commit

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 from pyscript import when
+from ultralytics import hub, YOLO
+from autodistill_grounded_sam import GroundedSAM
+from autodistill.detection import CaptionOntology
 
 @when("click", "button")
 def hello():
     print("hello")
+
+print(hub.login("86d48192211ece7b9fa5e0ff811f82e378538ca65c"))
